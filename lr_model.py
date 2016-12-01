65b0c74eb13c1d8eccd307a44fbdc087d86f9e4a
Fanxi Zeng <zengfancy@126.com>
2016-12-01 19:21:38 +0800

    lr_model.py modified

--- lr_model.py
+++ lr_model.py
@@ -1,4 +1,5 @@
 import math
+import feature
 
 def sigmoid(x):
   return 1 / (1 + exp(-x))
@@ -11,68 +12,71 @@ class SparseVec:
     self.index_val_map[index] = val
 
   def update_val(self, index, delta):
-    if self.index_val_map.has_key(index):
+    if index in self.index_val_map:
       self.index_val_map[index] += delta
     else:
       self.index_val_map[index] = delta
 
+  '''
+  @return: return 0 if not exist
+  '''
   def get_val(self, index):
-    if self.index_val_map.has_key(index):
+    if index in self.index_val_map:
       return self.index_val_map[index]
     else:
-      return None
+      return 0
 
 class LrModel:
   def __init__(self):
     self.weight_vec = SparseVec()
     self.b = 0
 
-  def update_params_mini_batch(self, batched_clicked, batched_features):
+  def update_params_mini_batch(self, batched_clicked, batched_features, learning_rate, lmbd):
     if len(batched_clicked) != len(batched_features):
       print("clicked num != features num. quit")
       return
     ys = []
 
     # learning rate
-    alpha = 0.001
+#    learning_rate = 0.001
     # regularization
-    lamb = 0.00001
+#    lmbd = 0.00001
 
     for features in batched_features:
       ys.append(self.infer(features))
     for i in range(len(ys)):
       delta = 0
       if not batched_clicked[i]:
-        delta = -alpha * ys[i]
+        delta = -learning_rate * ys[i]
       else:
-        delta = alpha * (1 - ys[i])
+        delta = learning_rate * (1 - ys[i])
       for feat in batched_features[i]:
         for val in feat.vals:
-          w_delta = delta - lamb * self.weight_vec.get_val(val.val)
+          w_delta = delta - lmbd * self.weight_vec.get_val(val.val)
           # confidence
           w_delta *= val.cfd
           self.weight_vec.update_val(val.val, w_delta)
-      self.b += delta - lamb * self.b
+      self.b += delta - lmbd * self.b
 
 
-  def update_params(self, clicked, features):
+  def update_params(self, clicked, features, learning_rate, lmbd):
     y = self.infer(features)
     # learning rate
-    alpha = 0.001
+#    learning_rate = 0.001
     delta = 0
     if not clicked:
-      delta = -alpha * y
+      delta = -learning_rate * y
     else:
-      delta = alpha * (1 - y)
+      delta = learning_rate * (1 - y)
     # regularization
-    lamb = 0.00001
+#    lmbd = 0.00001
     for feat in features:
       for val in feat.vals:
-        w_delta = delta - lamb * self.weight_vec.get_val(val.val)
+        w_delta = delta - lmbd * self.weight_vec.get_val(val.val)
         # confidence
         w_delta *= val.cfd
         self.weight_vec.update_val(val.val, w_delta)
-    self.b += delta - lamb * self.b
+    self.b += delta - lmbd * self.b
 
   def infer(self, features):
     weight_sum = 0
@@ -81,6 +85,11 @@ class LrModel:
         weight_sum += val.cfd * self.weight_vec.get_val(val.val)
     weight_sum += self.b
     return sigmoid(weight_sum)
+
+
+if __name__ == '__main__':
+
+
 
 
 
